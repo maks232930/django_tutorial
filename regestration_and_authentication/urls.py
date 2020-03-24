e752d7837af65c5e564c2984d201e5a980d5aa5a
maks <maksimpusklis7@gmail.com>
2020-03-24 21:40:09 +0300

    reset_password

--- regestration_and_authentication/urls.py
+++ regestration_and_authentication/urls.py
@@ -1,10 +1,30 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.homePage, name="home"),
     path('login/', views.loginPage, name="login"),
     path('register/', views.registerPage, name="register"),
-    path('logout/', views.logoutUser, name='logout')
+    path('logout/', views.logoutUser, name='logout'),
+
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(template_name="regestration_and_authentication/password_reset.html"),
+         name="reset_password"),
+
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name="regestration_and_authentication/password_reset_sent.html"),
+         name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name="regestration_and_authentication/password_reset_form.html"),
+         name="password_reset_confirm"),
+
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name="regestration_and_authentication/password_reset_done.html"),
+         name="password_reset_complete"),
 
 ]
